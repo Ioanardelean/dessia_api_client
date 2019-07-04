149857bf5738d269bfd0e695280c5cdddcac1dc5
Dumouchel <dumouchel@dessia.tech>
2019-07-04 22:46:14 +0200

    MAJ job_submit

--- dessia_api_client/core.py
+++ dessia_api_client/core.py
@@ -112,9 +112,9 @@ class Client:
         return r
 
 
-    def SubmitJob(self, job_type, input_data):
-        data = {'analysis_type': job_type,
-                'input_data': input_data}
+    def SubmitJob(self, Object, method):
+        data = {'object': Object,
+                'method': method}
         r = requests.post('{}/jobs/submit'.format(self.api_url),
                           headers=self.auth_header, json=data)
         return r
