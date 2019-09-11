c1c2eee0e7f5549e00e368a8be93fc4043120e33
Steven Masfaraud <masfaraud@dessia.tech>
2019-09-11 12:19:36 +0200

    Plot prices for product

--- dessia_api_client/core.py
+++ dessia_api_client/core.py
@@ -417,20 +417,20 @@ class Client:
                          proxies=self.proxies)
         return r
     
-    @retry_n_times
-    def request_get_manufacturers(self, limit, offset):
-        parameters = {'limit': limit, 'offset': offset}
-        r = requests.get('{}/marketplace/manufacturers'.format(self.api_url),
-                         params=parameters,
-                         headers=self.auth_header,
-                         proxies=self.proxies)
-        return r  
+#    @retry_n_times
+#    def request_get_manufacturers(self, limit, offset):
+#        parameters = {'limit': limit, 'offset': offset}
+#        r = requests.get('{}/marketplace/manufacturers'.format(self.api_url),
+#                         params=parameters,
+#                         headers=self.auth_header,
+#                         proxies=self.proxies)
+#        return r  
     
 #    def get_all_manufacturers(self):
 #        return self._get_all_elements('get_manufacturers')
     
-    def get_manufacturers(self, limit=20, offset=0):
-        r = self.request_get_manufacturers(limit, offset)
+    def get_manufacturers(self, limit=20, offset=0, filters=[]):
+        r = self.request_get_elements('manufacturer', limit, offset, filters=filters)
         return r.json()
 
 
@@ -672,7 +672,7 @@ class Client:
         current_time = int(time.time())
         filters = [EqualityFilter('sku.product.id', product_id)]
         price_offers = self.get_all_elements('price_offer', filters)
-        fig, ax = plt.subplots()
+        fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
         
         sku_ids = []
         sku_labels = {}
@@ -699,13 +699,13 @@ class Client:
             else:
                 validity_end = price_offer['validity_end']
             
-            handle, = ax.plot([price_offer['validity_start'], validity_end],
+            handle, = ax1.plot([price_offer['validity_start'], validity_end],
                     [price_offer['unit_price']]*2,
                     color=sku_colors[sku_id],
                     marker='o'
 #                        label=sku_labels[sku_id]
                     )
-            ax.text(0.5*(validity_end+price_offer['validity_start']),
+            ax1.text(0.5*(validity_end+price_offer['validity_start']),
                     price_offer['unit_price'],
                     '{}-{}'.format(price_offer['min_quantity'], price_offer['max_quantity'])
                     )
@@ -713,7 +713,52 @@ class Client:
                 handles.append(handle)
                 labels.append(sku_labels[sku_id])
                 labelled_sku.append(sku_id)
-        fig.legend(handles, labels)
+        ax1.legend(handles, labels)
+        ax1.set_title('Price offers')
 
 
-        ax.grid(True)
+        
+        product = self.get_element('product', product_id)
+        price_by_qty = {}
+        disappered_qty = set()
+        for timestamp, price_breaks in product['prices_history']:
+#            print(timestamp)
+            seen_qty_t = set()
+            for quantity, unit_price in price_breaks:
+                seen_qty_t.add(quantity)
+#                print(quantity, unit_price)
+                if quantity in disappered_qty:
+                    disappered_qty.remove(quantity)
+                if quantity in price_by_qty:
+                    price_by_qty[quantity][0].append(timestamp)
+                    price_by_qty[quantity][1].append(unit_price)
+                else:
+                    price_by_qty[quantity] = [[timestamp], [unit_price]]
+            
+            for quantity in price_by_qty.keys():
+                if not quantity in seen_qty_t:
+                    disappered_qty.add(quantity)
+
+            for quantity in disappered_qty:
+#                print('qtd', quantity)
+                price_by_qty[quantity][0].append(timestamp)                    
+                price_by_qty[quantity][1].append(None)                    
+
+        for quantity, (x,y) in price_by_qty.items():
+            if y[-1] is not None:
+                x.append(current_time)
+                y.append(y[-1])
+
+        nqty = len(price_by_qty)
+        for iq, (quantity, (x,y)) in enumerate(price_by_qty.items()):
+#            print(x,y)
+            color = cmap(iq/nqty)
+            ax2.step(x, y, where='post', label='For {}'.format(quantity), color=color)
+            ax2.plot(x, y, 'o', color=color)
+            
+        ax2.set_title('Global price history')
+        
+        ax2.legend()
+                    
+        ax1.grid(True)
+        ax2.grid(True)
